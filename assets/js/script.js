e87f20ad1ace851b760a429cc0758b201ad33fca
Shawn Cochran <shawn.cochran31@gmail.com>
2021-04-22 21:42:00 -0600

    initial skeleton, js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,31 @@
-var weather = 'hello';
-console.log(weather)
+fetch (
+    'https://api.openweathermap.org/data/2.5/onecall?lat=37.0965&lon=-113.5684&exclude=hourly,daily&appid=f44447ea0651e8a915b6acf9c822cdf5'
+)
+
+.then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  });
+
+var searchEl = document.querySelector('#search');
+var citySearch = document.querySelector('#city-search');
+var searchHistory = document.querySelector('#search-history');
+
+searchEl.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if (citySearch.value.length === '') return;
+    searchHistory.innerHTML += '<li>' + citySearch.value + '</li>';
+
+    citySearch.value = '';
+    
+    localStorage.setItem('citySearch', searchHistory.innerHTML);
+}, false);
+
+var saved = localStorage.getItem('citySearch');
+
+if (saved) {
+    searchHistory.innerHTML = saved;
+}
